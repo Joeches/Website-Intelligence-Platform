277f51ab7d75fe22c9d1bc20dc3dbda032fc6cbf
Joeches <77074250+Joeches@users.noreply.github.com>
2025-11-17 18:16:21 -0800

    Update scan_result_model.py

--- backend/models/scan_result_model.py
+++ backend/models/scan_result_model.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel
+from typing import Any, Dict, List
+
+class ScanResultModel(BaseModel):
+    url: str
+    title: str
+    h1: List[str]
+    keywords: List[str]
+    score: Dict[str, Any]
 
