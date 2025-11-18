29599474624f5300af7f6cc1fd545b5941a579d6
Joeches <77074250+Joeches@users.noreply.github.com>
2025-11-17 19:10:29 -0800

    Update PlanUpgradeBanner.tsx

--- frontend/components/PlanUpgradeBanner.tsx
+++ frontend/components/PlanUpgradeBanner.tsx
@@ -1,1 +1,9 @@
-
+export default function PlanUpgradeBanner() {
+return (
+<div className="p-5 bg-yellow-100 border-l-4 border-yellow-500 rounded">
+<p className="text-yellow-800 font-medium">
+You're on a limited plan. Upgrade to unlock full analytics + unlimited scans.
+</p>
+</div>
+);
+}
