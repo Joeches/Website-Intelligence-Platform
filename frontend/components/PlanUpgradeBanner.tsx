0954c4b24235a7fd9df3eb2f63aa5057884b1341
Joeches <77074250+Joeches@users.noreply.github.com>
2025-11-21 01:49:56 -0800

    Delete frontend directory

--- frontend/components/PlanUpgradeBanner.tsx
+++
@@ -1,9 +0,0 @@
-export default function PlanUpgradeBanner() {
-return (
-<div className="p-5 bg-yellow-100 border-l-4 border-yellow-500 rounded">
-<p className="text-yellow-800 font-medium">
-You're on a limited plan. Upgrade to unlock full analytics + unlimited scans.
-</p>
-</div>
-);
-}
